be6cfc0d0ce8894021f454a6054dfb31f84444b6
alikhanturusbekov <alikhan.jsjs@gmail.com>
2023-02-04 11:36:53 +0600

    Initial Commit

---
+++ Swiftcoin/SwiftcoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftcoinApp.swift
+//  Swiftcoin
+//
+//  Created by Alikhan Turusbekov on 04.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftcoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
